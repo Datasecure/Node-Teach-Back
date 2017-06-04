441cde83c3234b4e9637b8cff878cfee242900e3
iAmWillShepherd <williamrshepherd@gmail.com>
2017-06-04 15:51:20 -0500

    Add some notes

--- fun_with_files/read.ts
+++ fun_with_files/read.ts
@@ -2,6 +2,7 @@ import * as FS from 'fs'
 
 const file = 'movies.txt'
 
+// Read the docs: https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
 FS.readFile(file, 'utf8', (error, data) => {
   if (error) {
     console.error(error)
