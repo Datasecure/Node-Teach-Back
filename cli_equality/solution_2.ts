405e4d9cc4e1656e8d6b651612fa622d41cfef59
iAmWillShepherd <williamrshepherd@gmail.com>
2017-06-04 15:11:23 -0500

    Fix message

--- cli_equality/solution_2.ts
+++ cli_equality/solution_2.ts
@@ -1,6 +1,6 @@
 export default function refactoredEqualityTest() {
   // Note that while the LOC is smaller, the amount of duplication is greater
   // Remember that high-level programming languages are for humans; strive for readability and maintainability over terseness
-  console.log(`${parseInt(process.argv[2])} and ${parseInt(process.argv[3])} ${parseInt(process.argv[2]) === parseInt(process.argv[3]) ? 'are': 'are not.'}`)
-  console.log(`${parseInt(process.argv[2])} and ${parseInt(process.argv[3])} ${ (parseInt(process.argv[2]) + parseInt(process.argv[3])) % 7 === 0 ? 'are': 'are not' } multiples of 7`)
+  console.log(`${parseInt(process.argv[2])} and ${parseInt(process.argv[3])} ${parseInt(process.argv[2]) === parseInt(process.argv[3]) ? 'are': 'are not.'} equal`)
+  console.log(`${parseInt(process.argv[2])} and ${parseInt(process.argv[3])} ${(parseInt(process.argv[2]) + parseInt(process.argv[3])) % 7 === 0 ? 'are': 'are not'} multiples of 7`)
 }
