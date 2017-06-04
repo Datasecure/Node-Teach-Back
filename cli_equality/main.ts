a771d32b6e17ba27142b0ff6c07cf88ca5a3467b
iAmWillShepherd <williamrshepherd@gmail.com>
2017-06-04 15:41:43 -0500

    Group with rest of assignment

---
+++ cli_equality/main.ts
@@ -0,0 +1,7 @@
+import equalityTest from './soultion_1'
+import refactoredEqualityTest from './solution_2'
+
+console.log('Original version')
+equalityTest()
+console.log('Refactored version')
+refactoredEqualityTest()
--- main.ts
+++
@@ -1,7 +0,0 @@
-import equalityTest from './cli_equality/soultion_1'
-import refactoredEqualityTest from './cli_equality/solution_2'
-
-console.log('Original version')
-equalityTest()
-console.log('Refactored version')
-refactoredEqualityTest()
