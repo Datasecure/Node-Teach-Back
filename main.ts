d4a0be4f52001acbc0dc1cdf80bf4e71c11bbd88
iAmWillShepherd <williamrshepherd@gmail.com>
2017-06-04 15:11:41 -0500

    Test solutions

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 import equalityTest from './cli_equality/soultion_1'
 import refactoredEqualityTest from './cli_equality/solution_2'
 
+console.log('Original version')
 equalityTest()
+console.log('Refactored version')
+refactoredEqualityTest()
