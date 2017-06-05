41e3a9b189e17e94e1ee6fd2f69d475b9b2b490f
iAmWillShepherd <williamrshepherd@gmail.com>
2017-06-04 20:42:37 -0500

    Format output

--- bank_implementation/main.ts
+++ bank_implementation/main.ts
@@ -7,13 +7,17 @@ const bank = new Bank(file)
 
 switch (action.toUpperCase()) {
   case 'TOTAL':
-    bank.total().then((value) => console.log(value)).catch((reason) => console.error(reason))
+    bank.total()
+      .then((value) => console.log('$' + value))
+      .catch((reason) => console.error(reason))
     break;
   case 'DEPOSIT': {
     let a = parseInt(amount)
 
     if (a !== NaN) {
-      bank.deposit(a).catch((reason) => console.error(reason))
+      bank.deposit(a)
+        .then(_ => console.log(`Deposited $${a}`))
+        .catch((reason) => console.error(reason))
     }
 
     break
@@ -22,11 +26,15 @@ switch (action.toUpperCase()) {
     let a = parseInt(amount)
 
     if (a !== NaN) {
-      bank.withdraw(a).catch((reason) => console.error(reason))
+      bank.withdraw(a)
+        .then(_ => console.log(`Withdrew $${a}`))
+        .catch((reason) => console.error(reason))
     }
 
     break
   }
+  case 'LOTTO' :
+    break
   default:
     console.warn(`Unknown action: ${action}.`)
     break;
